0ccc35de1c436767c64e2c2eb0ffd15f17313cc6
grim1990 <grim1990@gmail.com>
2022-10-26 19:43:01 +0200

    bg-image

--- public/index.php
+++ public/index.php
@@ -1,5 +1,25 @@
-<?php
-declare(strict_types = 1); 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BestBarber</title>
 
-require __DIR__ . '/../src/Bootstrap.php';
-?>
+    <link rel="stylesheet" href="styles/bg-image.css">
+
+</head>
+<body>
+
+    NOWY TEKST
+
+
+   <!--  <?php
+   # declare(strict_types = 1);
+   # require __DIR__ . '/../src/Bootstrap.php';
+    ?> -->
+
+
+
+</body>
+</html>
