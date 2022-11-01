b45369e0f8bfb4cef94391c2beca6d21e33c7ccd
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-11-01 15:36:23 +0100

    login panel

--- public/login.php
+++ public/login.php
@@ -1,3 +1,7 @@
+<?php
+require('index.php');
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 
@@ -9,14 +13,11 @@
     <link rel="stylesheet" href="styles/app.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;500;700&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
@@ -32,17 +33,17 @@
                     <button class="small__button" onclick="window.location='/index.php';">✕</button>
                 </div>
                 <form class="user__forms" action="scripts/loginscript.php" method="post">
-                <div class="settings__fields">
-                       Login: <input class="input__fields" type="text" name="login">
-                </div>
-                <div class="settings__fields">
-                       Hasło: <input class="input__fields" type="password" name="password">
-                </div>
-                <div class="settings__fields">
-                    <div class="header mt-4">
-                       <input class="button__regular" type="submit" value="Zaloguj się">
+                    <div class="settings__fields">
+                        Login: <input class="input__fields" type="text" name="login">
                     </div>
-                </div>
+                    <div class="settings__fields">
+                        Hasło: <input class="input__fields" type="password" name="password">
+                    </div>
+                    <div class="settings__fields">
+                        <div class="header mt-4">
+                            <input class="button__regular" type="submit" value="Zaloguj się">
+                        </div>
+                    </div>
                 </form>
                 <form class="user__forms" action="/contact.php" method="post">
                     <div class="button-center mt-2">
@@ -52,15 +53,9 @@
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- public/register.php
+++ public/register.php
@@ -1,3 +1,7 @@
+<?php
+require('index.php');
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 
