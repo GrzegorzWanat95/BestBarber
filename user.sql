ed321ba187d383eda5295c719b6f0bcbf467ca2f
grim1990 <grim1990@gmail.com>
2022-11-03 18:19:48 +0100

    user.sql

--- user.sql
+++ user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 31 Paź 2022, 16:11
+-- Czas generowania: 03 Lis 2022, 18:19
 -- Wersja serwera: 10.4.24-MariaDB
 -- Wersja PHP: 8.1.6
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `test`
+-- Baza danych: `bestbarber`
 --
 
 -- --------------------------------------------------------
@@ -39,9 +39,25 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`ID`, `login`, `password`, `email`) VALUES
-(1, 'mateusz', '123', 'mateusz@email.com'),
-(2, 'grzegorz', '123', 'grzegorz@email.com'),
-(3, 'bartosz', '123', 'bartosz@email.com');
+(1, 'mateusz', '202cb962ac59075b964b07152d234b70', 'mateusz@email.com'),
+(2, 'grzegorz', '202cb962ac59075b964b07152d234b70', 'grzegorz@email.com'),
+(3, 'bartosz', '202cb962ac59075b964b07152d234b70', 'bartosz@email.com'),
+(7, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(8, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(9, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(10, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(11, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(12, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(13, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(14, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(15, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(16, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(17, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(18, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(19, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(20, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
+(21, 'aaa', '47bce5c74f589f4867dbd57e9ca9f808', 'aaa@email.com'),
+(22, 'a1', '8a8bb7cd343aa2ad99b7d762030857a2', 'a1@gmail.com');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -61,7 +77,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT dla tabeli `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
