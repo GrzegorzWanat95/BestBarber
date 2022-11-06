8e354eb2620605227b5bf5a4f255485d16ae77b7
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-11-06 08:17:54 +0100

    Edit service

--- public/edit-service.php
+++ public/edit-service.php
@@ -48,8 +48,8 @@ $id = $_GET["id"];
                         Cena: <input class="input__fields" type="number" name="price">
                     </div>
                     <div class="settings__fields">
-                        <?php if (isset($_SESSION['service-error'])) {
-                            foreach ($_SESSION['service-error'] as $error) {
+                        <?php if (isset($_SESSION['edit-service-error'])) {
+                            foreach ($_SESSION['edit-service-error'] as $error) {
                                 echo $error;
                                 break;
                             }
@@ -65,7 +65,7 @@ $id = $_GET["id"];
             </div>
         </div>
     </div>
-    <?php unset($_SESSION['service-error']) ?>
+    <?php unset($_SESSION['edit-service-error']) ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
--- public/scripts/edit-service.php
+++ public/scripts/edit-service.php
@@ -1,40 +1,57 @@
 <?php
-        require_once("connectdb.php");
-        //connect to database
-        $connection = new mysqli($host, $db_user, $db_password, $db_name);
-        $id = $_POST["id"];
-        $description = $_POST['description'];
-        $price = $_POST['price'];
 
-        echo($description);
-        echo($price);
-        echo($id);
-        //connection error
-        if ($connection->connect_error) {
-            die("Błąd połączenia z bazą danych: " . $connection->connect_error);
+    session_start();
+    require_once("connectdb.php");
+
+    $connection = new mysqli($host,$db_user,$db_password,$db_name);
+    $errors = array();
+
+    $id = $_POST["id"];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+
+    // form validation
+    $service_query_check = "SELECT * FROM services WHERE id='$id' LIMIT 1";
+    $result = mysqli_query($connection, $service_query_check);
+    $service = mysqli_fetch_assoc($result);
+
+    if ($service) { // if service exists
+        if ($service['description'] === $description) {
+          array_push($errors, "Usługa o podanej nazwie już istnieje!");
         }
-        else
-        {  
-          $errors = array();
-      
-          // form validation
-          $service_query_check = "SELECT * FROM services WHERE id='$id' LIMIT 1";
-          $result = mysqli_query($connection, $service_query_check);
-          $service = mysqli_fetch_assoc($result);
-          
-          if($service['id'] == $id)
-          {
-            $query = "UPDATE services SET description = '$description', price = '$price' WHERE id='$id'";
-            mysqli_query($connection, $query);
-            $_SESSION['success'] = "Edytowano usługę!";
-            header('location: ../price-list.php');
-          }
-          else
-          {
-            header('Location: ../price-list.php');
-          }
-    
-            $connection->close();      
+    }
 
-        } 
+    if (empty($description)) 
+    { 
+      array_push($errors, "Musisz podać nazwę usługi!");
+    }
+    if (empty($price)) 
+    { 
+      array_push($errors, "Musisz podać cenę usługi!");
+    }
+
+    if($service['id'] == $id)
+    {
+      
+    }
+    else
+    {
+      array_push($errors, "Nie znaleziono takiej usługi!");
+    }
+  
+    if(count($errors) == 0)
+    {
+      //Finally, register service if there are no errors in the form
+       unset($_SESSION['service-error']);
+       $query = "UPDATE services SET description = '$description', price = '$price' WHERE id='$id'";
+       mysqli_query($connection, $query);
+       $_SESSION['success'] = "Edytowano usługę!";
+       header('location: ../price-list.php');
+    }
+    else
+    {
+      $_SESSION['edit-service-error'] = $errors;
+      header('Location: ../edit-service.php');
+    }
+    $connection->close();           
 ?>
