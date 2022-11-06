e01169dc5c76ecb14935ef11c3dc2ac1b00c028b
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-11-06 16:07:15 +0100

    Review panel

--- public/add-opinion.php
+++ public/add-opinion.php
@@ -40,9 +40,6 @@ require('reviews.php');
                 </div>
                 <form class="user__forms" action="scripts/add-review.php" method="post">
                     <div class="settings__fields">
-                        Nick: <input class="input__fields" type="text" name="name">
-                    </div>
-                    <div class="settings__fields">
                         Ocena: <input class="input__fields" type="number" name="rating" min="1" max="5">
                     </div>
                     <div class="settings__fields__comments">
@@ -50,8 +47,8 @@ require('reviews.php');
                         <textarea class="input__fields" id="comment" type="number" rows="2" cols="50" name="content"></textarea>
                     </div>
                     <div class="settings__fields">
-                        <?php if (isset($_SESSION['service-error'])) {
-                            foreach ($_SESSION['service-error'] as $error) {
+                        <?php if (isset($_SESSION['add-review-error'])) {
+                            foreach ($_SESSION['add-review-error'] as $error) {
                                 echo $error;
                                 break;
                             }
@@ -67,7 +64,7 @@ require('reviews.php');
             </div>
         </div>
     </div>
-    <?php unset($_SESSION['service-error']) ?>
+    <?php unset($_SESSION['add-review-error']) ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
--- public/scripts/add-review.php
+++ public/scripts/add-review.php
@@ -6,36 +6,47 @@
     $connection = new mysqli($host,$db_user,$db_password,$db_name);
     $errors = array();
 
-    $name = $_POST['name'];
+    $name = $_SESSION['user'];
     $rating = $_POST['rating'];
     $content = $_POST['content'];
 
-    if (empty($name)) 
-    { 
-      array_push($errors, "Musisz podać swój nick!");
-    }
-    if (empty($rating)) 
-    { 
-      array_push($errors, "Musisz podać ocenę!");
-    }
-    if (empty($rating)) 
-    { 
-      array_push($errors, "Musisz podać treść opinii");
-    }
-  
-    if(count($errors) == 0)
+    if(!$connection)
     {
-      //Finally, register service if there are no errors in the form
-       unset($_SESSION['service-error']);
-       $query = "INSERT INTO reviews (name, content, rating)
-               VALUES('$name', '$content', '$rating')";
-       mysqli_query($connection, $query);
-       $_SESSION['success'] = "Dodano opinię!";
-       header('location: ../reviews.php');
+        error_reporting(0);
+        echo "Błąd połączenia z bazą danych";
     }
     else
     {
-      $_SESSION['service-error'] = $errors;
-      header('Location: ../add-opinion.php');
+      $user_check_query = "SELECT name FROM reviews WHERE name='$name' LIMIT 1";
+      $result = mysqli_query($connection, $user_check_query);
+      $user = mysqli_fetch_assoc($result);
+  
+      if ($user['name'] === $name) {
+        array_push($errors, "Użytkownik o podanej nazwie dodał już opinię!");
+      }
+
+      if (empty($rating)) 
+      { 
+        array_push($errors, "Musisz podać ocenę!");
+      }
+      if (empty($rating)) 
+      { 
+        array_push($errors, "Musisz podać treść opinii");
+      }
+    
+      if(count($errors) == 0)
+      {
+        //Finally, register service if there are no errors in the form
+         unset($_SESSION['add-review-error']);
+         $query = "INSERT INTO reviews (name, content, rating)
+                 VALUES('$name', '$content', '$rating')";
+         mysqli_query($connection, $query);
+         $_SESSION['success'] = "Dodano opinię!";
+         header('location: ../reviews.php');
+      }
+      else
+      {
+        $_SESSION['add-review-error'] = $errors;
+        header('Location: ../add-opinion.php');
+      }
     }
-?>
