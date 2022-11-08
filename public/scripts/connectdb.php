ff0a368d00da0995242c6c899b84304b60eb373d
grim1990 <grim1990@gmail.com>
2022-11-08 13:14:01 +0100

    connectdb

--- public/scripts/connectdb.php
+++ public/scripts/connectdb.php
@@ -1,6 +1,6 @@
 <?php
     $host="fdb30.awardspace.net";
     $db_user="4206533_bestbarber";
-    $db_password="Xf_9gVU5isPp_8b";
+    $db_password="QfxiHB7c666!";
     $db_name="4206533_bestbarber";
 ?>
