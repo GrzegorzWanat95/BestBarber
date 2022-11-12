8d8c7fa338330552f3be1c9b12c121bcaa844a99
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-11-12 20:21:58 +0100

    Reservation front

--- public/booking.php
+++ public/booking.php
@@ -19,7 +19,7 @@ require('scripts/time-range.php');
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Bootstra Datepicker CSS -->
     <link rel="stylesheet" href="assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css">
-   
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
@@ -27,11 +27,11 @@ require('scripts/time-range.php');
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   
+
 </head>
 
 <body>
-<div class="menu__top">
+    <div class="menu__top">
         <nav class="navbar navbar-expand-lg">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -86,104 +86,111 @@ require('scripts/time-range.php');
             </div>
         </div>
     </div>
-    <div >
-    <h1 class="header">REZERWACJE </h1>
-    <?php
-    if (isset($_SESSION['ADMIN']) && $_SESSION['ADMIN'] == true) { ?>
-         <form method="post" action="booking-script.php">
-                <div>
-                <div class="nativeDateTimePicker">
-                <input  type="date" id="date" name="date" required/>
-                </div>
-                <select name="hour" required>
-                    <option value="" disabled selected>Wybierz godzinę</option><br>
-                    <?php foreach($times as $key=>$val){ ?>
-                    <option value="<?php echo $val; ?>"><?php echo $val; ?></option>
-                    <?php } ?>
-                </select><br>
-                <select name="service" required >
-                    <option disabled selected>Wybierz usługę</option>;
-                    <?php while($rows = mysqli_fetch_array($result)){?>
-                    <option value="<?php echo $rows['description'];?>"><?php echo $rows ['description'];?></option>;
+    <div class="subpage__content">
+        <div class="content__frame">
+            <h1 class="header">
+                REZERWACJE
+            </h1>
+            <div class="holder">
+                <div class="half__side">
                     <?php
-                }
-                ?>
-                </select><br>
-                <button type="submit" name="book">Zarezerwuj</button>  
-                <div class="settings__fields">
-                        <?php if (isset($_SESSION['booking-error'])) {
-                            foreach ($_SESSION['booking-error'] as $error) {
-                                echo $error;
-                                break;
-                            }
-                        }
-                        ?>
-                    </div>
-                </div>
-            </form>
-    <?php } 
-    else {?>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Data</th>
-                    <th>Godzina</th>
-                    <th>Usługa</th>
-                    <th>Usuń</th>
-                    <th>Edycja</th>
-                    
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                // print all data from price-list.php
-                while ($row = $result_bookings->fetch_assoc()) { ?>
-                    <tr>
-                        <td><?php echo $row['id']; ?></td>
-                        <td><?php echo $row['date'] ; ?></td>
-                        <td><?php echo $row['hour'] ; ?></td>
-                        <td><?php echo $row['service'] ; ?></td>
-                        <!-- if (isset($_SESSION['ADMIN']) && $_SESSION['ADMIN'] == true) {  -->
-                        <td>
-                            <?php echo "<a href=scripts/delete-booking.php?id=" . $row['id'] . ">🗑</a>"; ?>
-                        </td>
-                        <td>
-                            <?php echo "<a href=edit-booking.php?id=" . $row['id'] . ">🖉</a>"; ?>
-                        </td>
-                            
-                        <?php } ?>
-                    </tr>
-            </tbody>
-        </table>
+                    if (isset($_SESSION['ADMIN']) && $_SESSION['ADMIN'] == true) { ?>
+                        <form method="post" action="booking-script.php">
+                            <div>
+                                <div class="nativeDateTimePicker">
+                                    <input type="date" id="date" name="date" required />
+                                </div>
+                                <select name="hour" required>
+                                    <option value="" disabled selected>Wybierz godzinę</option><br>
+                                    <?php foreach ($times as $key => $val) { ?>
+                                        <option value="<?php echo $val; ?>"><?php echo $val; ?></option>
+                                    <?php } ?>
+                                </select><br>
+                                <select name="service" required>
+                                    <option disabled selected>Wybierz usługę</option>;
+                                    <?php while ($rows = mysqli_fetch_array($result)) { ?>
+                                        <option value="<?php echo $rows['description']; ?>"><?php echo $rows['description']; ?></option>;
+                                    <?php
+                                    }
+                                    ?>
+                                </select><br>
+                                <button type="submit" name="book">Zarezerwuj</button>
+                                <div class="settings__fields">
+                                    <?php if (isset($_SESSION['booking-error'])) {
+                                        foreach ($_SESSION['booking-error'] as $error) {
+                                            echo $error;
+                                            break;
+                                        }
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                        </form>
+                    <?php } else { ?>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Data</th>
+                                <th>Godzina</th>
+                                <th>Usługa</th>
+                                <th>Usuń</th>
+                                <th>Edycja</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            // print all data from price-list.php
+                            while ($row = $result_bookings->fetch_assoc()) { ?>
+                                <tr>
+                                    <td><?php echo $row['id']; ?></td>
+                                    <td><?php echo $row['date']; ?></td>
+                                    <td><?php echo $row['hour']; ?></td>
+                                    <td><?php echo $row['service']; ?></td>
+                                    <!-- if (isset($_SESSION['ADMIN']) && $_SESSION['ADMIN'] == true) {  -->
+                                    <td>
+                                        <?php echo "<a href=scripts/delete-booking.php?id=" . $row['id'] . ">🗑</a>"; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo "<a href=edit-booking.php?id=" . $row['id'] . ">🖉</a>"; ?>
+                                    </td>
 
-    <?php    
-    }?>
+                                <?php } ?>
+                                </tr>
+                        </tbody>
+                    </table>
+                    <?php
+                    } ?>
+                    <?php
+                    if (isset($_SESSION['ADMIN']) && $_SESSION['ADMIN'] == true) { ?>
+                        <a link href="add-service.php">
+                            <div class="button__field__xxl">
+                                <p1 class="button__text__table p-1">Dodaj usługę</p1>
+                            </div>
+                        </a>
+                    <?php } ?>
+                </div>
+                <div class="half__side">
+                    <img class="price" src="../img/priceList.png" alt="Zdjęcie narzędzi fryzjerskich">
+                </div>
+            </div>
+        </div>
     </div>
-    
-            
-            
-           
-            
-   
-   
-    
-    
-        <div class="footer">
-            <img class="logo__footer" src="../img/logo1biel.png" alt="BestBarber logo">
-            <div class="footer__text">
-             Copyright©2022 BestBarber
+    <div class="footer">
+        <img class="logo__footer" src="../img/logo1biel.png" alt="BestBarber logo">
+        <div class="footer__text">
+            Copyright©2022 BestBarber
         </div>
     </div>
     <script>
         const picker = document.getElementById('date');
-        picker.addEventListener('input', function(e){
-        var day = new Date(this.value).getUTCDay();
-        if([6,0].includes(day)){
-            e.preventDefault();
-            this.value = '';
-            alert('W weekendy jesteśmy zamknięci');
-        }
+        picker.addEventListener('input', function(e) {
+            var day = new Date(this.value).getUTCDay();
+            if ([6, 0].includes(day)) {
+                e.preventDefault();
+                this.value = '';
+                alert('W weekendy jesteśmy zamknięci');
+            }
         });
     </script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
