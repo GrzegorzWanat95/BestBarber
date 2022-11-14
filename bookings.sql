0ef0ac946d7b692d0b312a22dec8f261e221b946
grim1990 <grim1990@gmail.com>
2022-11-14 19:09:41 +0100

    sql

--- bookings.sql
+++ bookings.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 10 Lis 2022, 00:43
--- Wersja serwera: 10.4.25-MariaDB
--- Wersja PHP: 8.1.10
+-- Czas generowania: 14 Lis 2022, 19:09
+-- Wersja serwera: 10.4.24-MariaDB
+-- Wersja PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,19 +31,16 @@ CREATE TABLE `bookings` (
   `id` int(10) NOT NULL,
   `date` text NOT NULL,
   `hour` text NOT NULL,
-  `service` text NOT NULL
+  `service` text NOT NULL,
+  `username` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `bookings`
 --
 
-INSERT INTO `bookings` (`id`, `date`, `hour`, `service`) VALUES
-(29, '2022-11-09', '13:00', 'Strzyżenie włosów + golenie karku brzytwą'),
-(30, '2022-11-24', '10:00', 'Strzyżenie włosów + golenie karku brzytwą'),
-(32, '2022-11-24', '18:00', 'Strzyżenie włosów + golenie karku brzytwą'),
-(33, '2022-11-24', '13:00', 'Strzyżenie włosów + golenie karku brzytwą'),
-(38, '2022-11-26', '11:00', 'Golenie pełne głowy brzytwą');
+INSERT INTO `bookings` (`id`, `date`, `hour`, `service`, `username`) VALUES
+(53, '2022-11-14', '10:00', 'Strzyżenie brody nożyczkami / brzytwą', 'mateusz');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -63,7 +60,7 @@ ALTER TABLE `bookings`
 -- AUTO_INCREMENT dla tabeli `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
