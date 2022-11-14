abf4c772e5a5ff5e8e0265022fbca48a25ec0b65
grim1990 <grim1990@gmail.com>
2022-11-14 14:02:04 +0100

    user: add-booking

--- public/scripts/add-booking.php
+++ public/scripts/add-booking.php
@@ -1,5 +1,5 @@
 <?php
-
+    session_start();
     require_once("connectdb.php");
 
     $connection = new mysqli($host,$db_user,$db_password,$db_name);
@@ -16,19 +16,19 @@
       $date = $_POST['date'];
       $hour = $_POST['hour'];
       $service_name = $_POST['service'];
+      $username =  $_SESSION['user'];
 
       if(empty($service_name))
       {
         array_push($errors, "Musisz wybrać rodzaj usługi!");
       }
 
-
       if(count($errors) == 0)
       {
         //Finally, register service if there are no errors in the form
         unset($_SESSION['service-error']);
-        $query = "INSERT INTO bookings (date, hour, service)
-        VALUES('$date', '$hour', '$service_name')";
+        $query = "INSERT INTO bookings (date, hour, service, username)
+        VALUES('$date', '$hour', '$service_name', '$username')";
         mysqli_query($connection, $query);
         $_SESSION['success'] = "Zarezerwowano termin!";
         header('location: ../add-reservation.php');
@@ -38,6 +38,6 @@
         $_SESSION['edit-service-error'] = $errors;
         header('Location: ../booking-date.php');
       }
-      $connection->close();        
+      $connection->close();
     }
 ?>
