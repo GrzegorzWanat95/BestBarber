dae1a0c434a0952407338d355cbd4dab12168acb
grim1990 <grim1990@gmail.com>
2022-11-14 21:01:51 +0100

    check-booking

---
+++ public/scripts/check-booking.php
@@ -0,0 +1,27 @@
+<?php
+    require_once("connectdb.php");
+    $username = $_SESSION['user'];
+    $connection = new mysqli($host, $db_user, $db_password, $db_name);
+    $query = "SELECT * FROM bookings WHERE username='$username' ORDER BY hour DESC";
+        if ($result = $connection->query($query))
+        {
+            while ($row = $result->fetch_assoc())
+            {
+                $date = $row["date"];
+                $hour = $row["hour"];
+                $service = $row["service"];
+
+                echo
+                '<tr>
+                    <td>'.$date.'</td>
+                    <td>'.$hour.'</td>
+                    <td>'.$service.'</td>
+                </tr>';
+            }
+            $result->free();
+        }
+        else
+        {
+            echo '<tr> Brak wizyt </tr>';
+        }
+?>
--- public/user-panel.php
+++ public/user-panel.php
@@ -135,29 +135,7 @@ require('./scripts/userscript.php');
                             </tr>
                         </thead>
                         <tbody>
-                            <?php
-                                $username = $_SESSION['user'];
-                                $connection = new mysqli($host, $db_user, $db_password, $db_name);
-                                $query = "SELECT * FROM bookings WHERE username='$username' ORDER BY hour DESC";
-                                if ($result = $connection->query($query)) {
-                                    while ($row = $result->fetch_assoc()) {
-                                        $date = $row["date"];
-                                        $hour = $row["hour"];
-                                        $service = $row["service"];
-
-                                        echo '<tr>
-                                                <td>'.$date.'</td>
-                                                <td>'.$hour.'</td>
-                                                <td>'.$service.'</td>
-                                            </tr>';
-                                    }
-                                    $result->free();
-                                }
-                                else
-                                {
-                                    echo '<tr> Brak wizyt </tr>';
-                                }
-                            ?>
+                            <?php include 'scripts/check-booking.php'; ?>
                         </tbody>
                     </table>
                 </div>
