e5a190cec233b43bab1a6088f5fda5b484369437
grim1990 <grim1990@gmail.com>
2022-11-14 20:37:50 +0100

    user-panel

--- public/user-panel.php
+++ public/user-panel.php
@@ -136,8 +136,9 @@ require('./scripts/userscript.php');
                         </thead>
                         <tbody>
                             <?php
+                                $username = $_SESSION['user'];
                                 $connection = new mysqli($host, $db_user, $db_password, $db_name);
-                                $query = "SELECT * FROM bookings ORDER BY hour DESC";
+                                $query = "SELECT * FROM bookings WHERE username='$username' ORDER BY hour DESC";
                                 if ($result = $connection->query($query)) {
                                     while ($row = $result->fetch_assoc()) {
                                         $date = $row["date"];
@@ -151,6 +152,10 @@ require('./scripts/userscript.php');
                                             </tr>';
                                     }
                                     $result->free();
+                                }
+                                else
+                                {
+                                    echo '<tr> Brak wizyt </tr>';
                                 }
                             ?>
                         </tbody>
