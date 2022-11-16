0319e1d2157b24798d8803780a9dbb80cebd84f7
grim1990 <grim1990@gmail.com>
2022-11-16 12:34:31 +0100

    cleanup

--- public/scripts/menu-collapse-script.php
+++ public/scripts/menu-collapse-script.php
@@ -1,18 +1,18 @@
 <?php
     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
     {
-        echo '<a class="nav-item text-light nav-link" href="user-panel.php">WITAJ '.strtoupper($_SESSION["user"]).' !</a>';
-        echo '<a class="nav-item text-light nav-link" href="scripts/logout.php">WYLOGUJ</a>';
+        echo '<a class="nav-item text-light nav-link" href="user-panel.php">WITAJ '.strtoupper($_SESSION["user"]).' !</a>
+              <a class="nav-item text-light nav-link" href="scripts/logout.php">WYLOGUJ</a>';
     }
     else
     {
-        echo '<a class="nav-item text-light nav-link" href="login.php">LOGOWANIE</a>';
-        echo '<a class="nav-item text-light nav-link" href="register.php">REJESTRACJA</a>';
+        echo '<a class="nav-item text-light nav-link" href="login.php">LOGOWANIE</a>
+              <a class="nav-item text-light nav-link" href="register.php">REJESTRACJA</a>';
     }
-        echo '<a class="nav-item nav-link" href="about.php">O NAS</a>';
-        echo '<a class="nav-item nav-link" href="price-list.php">CENNIK</a>';
-        echo '<a class="nav-item nav-link" href="contact.php">KONTAKT</a>';
-        echo '<a class="nav-item nav-link" href="reviews.php">OPINIE</a>';
+        echo '<a class="nav-item nav-link" href="about.php">O NAS</a>
+              <a class="nav-item nav-link" href="price-list.php">CENNIK</a>
+              <a class="nav-item nav-link" href="contact.php">KONTAKT</a>
+              <a class="nav-item nav-link" href="reviews.php">OPINIE</a>';
 
     if (isset($_SESSION['ADMIN']) && $_SESSION['ADMIN'] == true)
     {
