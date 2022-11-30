d2a45d668da9c23b172d0cd88b089179ea58a12f
GrzegorzWanat95 <grzegorzwanat@protonmail.com>
2022-11-30 08:10:34 +0100

    final fixes

--- view/home/contact.php
+++ view/home/contact.php
@@ -21,7 +21,7 @@ session_start();
 </head>
 
 <body>
-<div class="menu__top">
+    <div class="menu__top">
         <nav class="navbar navbar-expand-lg">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
