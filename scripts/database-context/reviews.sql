9f78891522a72ff85fac0c04e61cf7d1f54d3ca5
Ynfus <95777380+Ynfus@users.noreply.github.com>
2022-12-02 21:32:52 +0100

    reviews from MyAdmin

--- scripts/database-context/reviews.sql
+++ scripts/database-context/reviews.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Czas generowania: 08 Lis 2022, 12:43
--- Wersja serwera: 10.4.24-MariaDB
--- Wersja PHP: 8.1.6
+-- Host: 127.0.0.1:3306
+-- Czas generowania: 02 Gru 2022, 21:30
+-- Wersja serwera: 10.5.15-MariaDB-10+deb11u1
+-- Wersja PHP: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `bestbarber`
+-- Baza danych: `ynfus`
 --
 
 -- --------------------------------------------------------
@@ -29,35 +30,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `reviews` (
   `id` int(11) NOT NULL,
-  `page_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `content` text NOT NULL,
   `rating` tinyint(1) NOT NULL,
   `submit_date` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Zrzut danych tabeli `reviews`
 --
 
-INSERT INTO `reviews` (`id`, `page_id`, `name`, `content`, `rating`, `submit_date`) VALUES
-(1, 1, 'David Deacon', 'I use this website daily, the amount of content is brilliant.', 5, '2022-01-05 20:43:02'),
-(2, 1, 'John Doe', 'Great website, great content, and great support!', 4, '2020-01-09 21:00:41'),
-(3, 1, 'Robert Billings', 'Website needs more content, good website but content is lacking.', 3, '2020-01-09 21:10:16'),
-(4, 1, 'Daniel Callaghan', 'Great!', 5, '2020-01-09 23:51:05'),
-(5, 1, 'Bobby', 'Not much content.', 2, '2020-01-14 21:54:24'),
-(6, 1, 'Joshua Kennedy', 'Fantasic website, has everything I need to know.', 5, '2020-01-16 17:34:27'),
-(7, 1, 'Johannes Hansen', 'Really like this website, helps me out a lot!', 5, '2020-01-16 17:35:12'),
-(8, 1, 'Wit Kwiatkowski', 'Please provide more quality content.', 5, '2020-01-16 17:36:03'),
-(9, 1, 'Ã“li ÃžÃ³rÃ°arson', 'Thanks', 5, '2020-01-16 17:36:34'),
-(10, 1, 'Jaroslava BerÃ¡nkovÃ¡', 'saaaaaaaaaaaa', 5, '2020-01-16 17:37:48'),
-(11, 1, 'Naomi Holt', 'Appreciate the amount of content you guys do.', 5, '2020-01-16 17:39:17'),
-(12, 1, 'Isobel Whitehead', 'Thank you for providing a website that helps us out a lot!', 5, '2020-01-16 17:40:28'),
-(13, 1, 'Warren Mills', 'Everything is awesome!', 5, '2020-01-16 19:34:08'),
-(14, 1, 'Larry Johnson', 'Brilliant, thank you for providing quality content!', 5, '2020-01-29 18:40:36'),
-(15, 1, 'adaa', 'nice page', 4, '2022-11-02 13:44:37'),
-(17, 1, 'sad', 'sad', 2, '2022-11-03 07:17:32'),
-(18, 1, 'Sandals Problem', 'Bo TAK!', 1, '2022-11-03 18:25:36');
+INSERT INTO `reviews` (`id`, `name`, `content`, `rating`, `submit_date`) VALUES
+(26, 'mateusz', 'Super!', 5, '2022-11-29 19:10:59'),
+(25, 'Paweł', 'Piękne cięcie!', 5, '2022-11-29 09:15:38'),
+(24, 'grzegorzwanat', 'Wspaniałe miejsce. Szkoda, że trudno o wolny termin. ', 4, '2022-11-29 09:12:48'),
+(23, 'Patrycja', 'Fajna stronka, bardzo ładna', 5, '2022-11-26 15:23:11');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -77,7 +64,7 @@ ALTER TABLE `reviews`
 -- AUTO_INCREMENT dla tabeli `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
